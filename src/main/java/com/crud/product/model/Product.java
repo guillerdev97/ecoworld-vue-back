53dc9b42fdc6e27c320d1a723dc9db9dca35c58b
Guillermo García <guillermieres97@gmail.com>
2022-09-14 23:54:45 +0200

    create product model and set products table

---
+++ src/main/java/com/crud/product/model/Product.java
@@ -0,0 +1,24 @@
+package com.crud.product.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "product_name")
+    private String name;
+    @Column(name = "product_cost")
+    private Float price;
+    @Column(name = "product_description")
+    private String description;
+}
